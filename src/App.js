e0d36f236302580dabc1521c130bc374933deb2c
KristianMeier <85857919+KristianMeier@users.noreply.github.com>
2022-07-17 10:47:06 +0200

    back to start setup

--- src/App.js
+++ src/App.js
@@ -5,41 +5,6 @@ import Tours from "./Tours";
 // I SWITCHED TO PERMANENT DOMAIN
 const url = "https://course-api.com/react-tours-project";
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [tours, setTours] = useState([]);
-
-  const fetchTours = async () => {
-    setLoading(true);
-
-    try {
-      const response = await fetch(url);
-      const tours = await response.json();
-      setLoading(false);
-      setTours(tours);
-    } catch (error) {
-      setLoading(false);
-      console.log(error);
-    }
-  };
-
-  useEffect(() => {
-    fetchTours();
-  }, []);
-
-  if (loading == true) {
-    return (
-      <main>
-        <Loading />
-      </main>
-    );
-  }
-
-  return (
-    <main>
-      <Tours />
-    </main>
-  );
-
   return <h2>Tours Project Setup</h2>;
 }
 
