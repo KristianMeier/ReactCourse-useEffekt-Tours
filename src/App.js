0ebf9f18e8433983a651061204fc5173329ab308
KristianMeier <85857919+KristianMeier@users.noreply.github.com>
2022-07-17 11:11:12 +0200

    fuld video 1, med commentare

--- src/App.js
+++ src/App.js
@@ -5,7 +5,46 @@ import Tours from "./Tours";
 // I SWITCHED TO PERMANENT DOMAIN
 const url = "https://course-api.com/react-tours-project";
 function App() {
-  return <h2>Tours Project Setup</h2>;
+  // Set usestates
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  //Fetch tours
+  //try: resposen, tours setLoading, setTours
+  //catch:
+  const fetchTours = async () => {
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
+  //useEffect, FetchTours
+  useEffect(() => {
+    fetchTours();
+    console.log(tours);
+  }, []);
+
+  //If loading, laoding
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+
+  //return Tours
+  return (
+    <main>
+      <Tours />
+    </main>
+  );
 }
 
 export default App;
