095a19e8a2bb99529be81bb7943666d30ec58fb2
KristianMeier <85857919+KristianMeier@users.noreply.github.com>
2022-07-19 23:09:31 +0200

    Lille ændring.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
 import Loading from "./Loading";
 import Tours from "./Tours";
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
+
 const url = "https://course-api.com/react-tours-project";
+
 function App() {
   //1. set useStates loading og tours
   const [loading, setLoading] = useState(true);
